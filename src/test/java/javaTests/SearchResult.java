0f87dba6eaac536e503c56dd53a5af5e819d88a9
viktoriyakosheleva <viktoriyanov63@gmail.com>
2024-04-08 18:35:34 +0400

    HW1-done

---
+++ src/test/java/javaTests/SearchResult.java
@@ -0,0 +1,19 @@
+package javaTests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class SearchResult {
+        @Test
+        void successfulSearchTestG() {
+            open("https://www.google.com/");
+            $("[name=q]").setValue("selenide").pressEnter();
+            $("[id=search]").shouldHave(text("https://selenide.org"));
+
+
+        }
+    }
